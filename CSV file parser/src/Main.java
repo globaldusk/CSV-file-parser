7b2ce2b6a8f2c1020e3416ec761dc3ba31804728
globaldusk <67882245+globaldusk@users.noreply.github.com>
2022-12-06 23:20:08 +1100

    file scanner

--- CSV file parser/src/Main.java
+++ CSV file parser/src/Main.java
@@ -4,25 +4,37 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+import static java.lang.Character.getNumericValue;
+
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
+        Character a = 'z';
+        System.out.println(getNumericValue(a)-9);
+        System.out.println("Hello world!");//how to find cell id with letters
 
-        System.out.println("Hello world!");
+        List<String> rows = new ArrayList<>();
+        List<String> data = new ArrayList<>();
 
-        List<List<String>> dataMatrix = new ArrayList<List<String>>();
-
-        //parsing a CSV file into Scanner class constructor
         Scanner sc = new Scanner(new File("example csv.csv"));
-        sc.useDelimiter(",");   //sets the delimiter pattern
-        while (sc.hasNext())  //returns a boolean value
+        while (sc.hasNextLine())  //returns a boolean value
         {
-            System.out.print(sc.next());  //find and returns the next complete token from this scanner
-            if(sc.hasNext()){            dataMatrix.add(Arrays.asList(sc.next()));System.out.print(",");}
-
+            //System.out.print(sc.next());  //find and returns the next complete token from this scanner
+            rows.add(sc.nextLine());
         }
         sc.close();  //closes the scanner
 
-       // System.out.println(dataMatrix);
+        Scanner scanner = new Scanner(new File("example csv.csv"));
+        scanner.useDelimiter(",");   //sets the delimiter pattern
+        while (scanner.hasNext())  //returns a boolean value
+        {
+            //System.out.print(sc.next());  //find and returns the next complete token from this scanner
+            data.add(scanner.next());
+        }
+        scanner.close();  //closes the scanner
 
+        for(int i = 0; i < data.size(); i++){
+            System.out.println(data.get(i));
+        }
     }
+
 }
