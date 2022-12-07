bf3a933654ada65fcacf6527e8994c6f8c6a32c9
globaldusk <67882245+globaldusk@users.noreply.github.com>
2022-12-07 20:06:37 +1100

    gettiong close. will do multiple calcs in same cell

--- CSV file parser/src/Main.java
+++ CSV file parser/src/Main.java
@@ -1,15 +1,13 @@
 import java.io.*;
 import java.util.*;
 
-import static java.lang.Character.getNumericValue;
+import static java.lang.Character.*;
 
 public class Main {
+    public static List<List<String>> cellMatrix = new ArrayList<>();
     public static void main(String[] args) throws FileNotFoundException {
-        Character a = 'z';
-        System.out.println(getNumericValue(a)-9);
-        System.out.println("Hello world!");//how to find cell id with letters
 
-        List<List<String>> cellMatrix = new ArrayList<>();
+
         List<String> rows = new ArrayList<>();
 
         Scanner sc = new Scanner(new File("example csv.csv"));
@@ -29,6 +27,7 @@ public class Main {
             for (String cell : list) {
                 System.out.print(cell+" -> ");
                 System.out.println(evaluatePostfix(cell));
+                list.set(list.indexOf(cell), evaluatePostfix(cell));
             }
         }
         System.out.println(cellMatrix);
@@ -46,35 +45,71 @@ public class Main {
         }
     }
 
-    static int evaluatePostfix(String exp)
+    public static String findCellVal(String coord){
+        int row = 0;
+        int col = 0;
+
+        for(int i=0;i<coord.length();i++) {
+            char c = coord.charAt(i);
+
+            if (Character.isLetter(c)) {
+                col= getNumericValue(c)-10;
+            }
+
+            else if (Character.isDigit(c)) {
+                row = (c - '0')-1;
+            }
+
+            else {
+                return "#ERR";
+            }
+        }
+        return (cellMatrix.get(row)).get(col);
+    }
+
+    static String evaluatePostfix(String exp)
     {
+
         //create a stack
-        Stack<Integer> stack=new Stack<>();
+        Stack<String> stack=new Stack<>();
         String[] items = exp.split(" ");
 
         // Scan all characters one by one
         for(String item : items)
         {
-
-            if(isNumeric(item)){
-                stack.push(Integer.parseInt(item));
-            }
-
-
-                //  If the scanned character is an operator, pop two
-                // elements from stack apply the operator
-            else
-            {
-                int val1 = stack.pop();
-                int val2 = stack.pop();
-
-                switch (item) {
-                    case "+" -> stack.push(val2 + val1);
-                    case "-" -> stack.push(val2 - val1);
-                    case "/" -> stack.push(val2 / val1);
-                    case "*" -> stack.push(val2 * val1);
+            boolean letterNumber = true;
+            char[] itemParts = item.toCharArray();
+            for(char chars : itemParts){
+                if(!Character.isLetterOrDigit(chars)){
+                    letterNumber = false;
                 }
             }
+
+            if(isNumeric(item)){
+                stack.push(item);
+            }
+
+            else if(letterNumber){//if is letter number
+                System.out.println("HERE-"+findCellVal(item));
+                stack.push(findCellVal(item));
+            }
+                //  If the scanned character is an operator, pop two
+                // elements from stack apply the operator
+            else{
+                int val1 = Integer.parseInt(stack.pop());
+                int val2 = Integer.parseInt(stack.pop());
+                switch (item) {
+                    case "+" -> stack.push(Integer.toString(val2 + val1));
+                    case "-" -> stack.push(Integer.toString(val2 - val1));
+                    case "/" -> stack.push(Integer.toString(val2 / val1));
+                    case "*" -> stack.push(Integer.toString(val2 * val1));
+                    default -> {
+                        System.out.print(" HERE ");
+                        return "#ERR";
+                    }
+                }
+            }
+
         }
         //System.out.print(stack.pop());
         return stack.pop();
